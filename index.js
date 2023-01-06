ae1638973b0a8e82b345ff73e891c31b87fd7420
emilytm <emilytmcintyre@gmail.com>
2023-01-06 18:07:44 -0500

    Got badges showing up when button is clicked

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import { Dog } from './Dog.js'
 
 
 let Teddy = new Dog(dogs.find(dog => dog.name === "Teddy"))
-const interactionBadge = document.getElementById('interaction-badge')
 const likeButton = document.getElementById('like-button')
 const dislikeButton = document.getElementById('dislike-button')
 
@@ -13,25 +12,15 @@ likeButton.addEventListener('click',likeDog)
 dislikeButton.addEventListener('click', dislikeDog)
 
 function likeDog(){
-    console.log('like button clicked')
+    let interactionBadge = document.getElementById('interaction-badge')
     interactionBadge.setAttribute('src','../images/badge-like.png')
     interactionBadge.classList.remove('hidden')
 }
 
 function dislikeDog(){
-    console.log('dislike button clicked')
+    let interactionBadge = document.getElementById('interaction-badge')
     interactionBadge.setAttribute('src','../images/badge-nope.png')
     interactionBadge.classList.remove('hidden')
-}
-
-function displayBadge(){
-    
-}
-
-function getBadgeHtml(interaction){
-        return `
-            <img src="../images/badge-${interaction}.png" alt="${interaction} badge">
-        `
 }
 
 //Display the first dog
