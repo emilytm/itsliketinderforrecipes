f21c1e2dd1fe5f153b73c73901c386793448d7e1
emilytm <emilytmcintyre@gmail.com>
2023-01-10 14:24:51 -0500

    Removing unused variables

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import { dogs } from './data.js'
 import { Dog } from './Dog.js'
 
 
-
-let Teddy = new Dog(dogs.find(dog => dog.name === "Teddy"))
+let currentDog = new Dog(dogs.shift())
 const likeButton = document.getElementById('like-button')
 const dislikeButton = document.getElementById('dislike-button')
 
@@ -12,12 +11,10 @@ likeButton.addEventListener('click',likeDog)
 dislikeButton.addEventListener('click', dislikeDog)
 
 function likeDog(){
-
     flashBadge(1)
 }
 
 function dislikeDog(){
-
     flashBadge(0)
 }
 
@@ -56,4 +53,4 @@ function renderDog(Dog){
     dogDisplaySection.innerHTML = Dog.getDogHtml()
 }
 
-renderDog(Teddy)
+renderDog(currentDog)
